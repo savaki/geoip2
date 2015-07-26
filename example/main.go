415c02e7357deeae86552ed8ea47f74d39d06371
savaki <matt.ho@gmail.com>
2015-07-26 11:24:50 -0700

    changed sample ip address

--- example/main.go
+++ example/main.go
@@ -9,6 +9,6 @@ import (
 
 func main() {
 	api := geoip2.New(os.Getenv("MAXMIND_USER_ID"), os.Getenv("MAXMIND_LICENSE_KEY"))
-	resp, _ := api.City(nil, "1.2.3.4")
+	resp, _ := api.City(nil, "8.8.8.8")
 	json.NewEncoder(os.Stdout).Encode(resp)
 }
